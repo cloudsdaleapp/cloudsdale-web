c6d8f1c9370eda624ef5944f62243b36dc6b1376
Philip Vieira <philip@vallin.se>
2011-09-22 15:14:45 +0200

    Trying bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Cloudsdale::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # 
+  config.assets.precompile = %w(*.png *.jp?g *.gif *.css *.js)
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
