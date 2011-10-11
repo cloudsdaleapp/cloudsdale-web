c96b78f3a76241bc18933bc6438a3d3a7e220096
Philip Vieira <philip@vallin.se>
2011-10-11 02:58:03 +0200

    fixed message parsing to work properly with multiline strings.

--- app/controllers/chat/messages_controller.rb
+++ app/controllers/chat/messages_controller.rb
@@ -13,6 +13,9 @@ class Chat::MessagesController < ApplicationController
     t = Time.now
     
     message.gsub! /<\/?[^>]*>/, ""
+    message.gsub! /[\u000d\u0009\u000c\u0085\u2028\u2029\n]/, "<br/>"
+    message.gsub! /<br\/><br\/>/,"<br/>"
+    message.gsub! /^\s*$/, ""
     
     if (m = message.match(/^(.*)/i))
       msg = m[0].gsub /([a-z]{1,6}\:\/\/)([a-z0-9\.\,\-\_]*)(\/?[a-z0-9\.\,\-\_\/\?\&\=]*)/i do
@@ -24,11 +27,12 @@ class Chat::MessagesController < ApplicationController
       end
     end
     
-    if Chat::Message.create(sender: sender, timestamp: t, content: msg)
+    
+    if Chat::Message.create(sender: sender, timestamp: t, content: msg) and !msg.empty?
       render :partial => 'create.js.erb',
       :locals => {
                     :sender => sender,
-                    :message => msg.html_safe,
+                    :message => msg,
                     :timestamp => t.to_js
                   }
     end
