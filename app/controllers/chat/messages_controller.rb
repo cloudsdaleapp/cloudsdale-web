3cd3bc05d2615864ee43a21e650c2c12fff6f99a
Philip Vieira <philip@vallin.se>
2011-10-12 21:06:18 +0200

    Added a lot of logic from the chat#messages controller to the chat message model.

--- app/controllers/chat/messages_controller.rb
+++ app/controllers/chat/messages_controller.rb
@@ -8,33 +8,17 @@ class Chat::MessagesController < ApplicationController
   end
   
   def create
-    sender = session[:display_name]
-    message = params[:message]
-    t = Time.now
+    @message = Chat::Message.new(sender: session[:display_name], content: params[:message])
     
-    message.gsub! /<\/?[^>]*>/, ""
-    message.gsub! /[\u000d\u0009\u000c\u0085\u2028\u2029\n]/, "<br/>"
-    message.gsub! /<br\/><br\/>/,"<br/>"
-    message.gsub! /^\s*$/, ""
-    
-    if (m = message.match(/^(.*)/i))
-      msg = m[0].gsub /([a-z]{1,6}\:\/\/)([a-z0-9\.\,\-\_]*)(\/?[a-z0-9\.\,\-\_\/\?\&\=]*)/i do
-        protocol = $1
-        top_dom = $2
-        path = $3
-        url = protocol + top_dom + path
-        "<a class='chat-link' href='#{url}' target='_blank'>#{url}</a>"
-      end
-    end
-    
-    
-    if Chat::Message.create(sender: sender, timestamp: t, content: msg) and !msg.empty?
+    if @message.save
       render :partial => 'create.js.erb',
       :locals => {
-                    :sender => sender,
-                    :message => msg,
-                    :timestamp => t.to_js
+                    :sender => @message.sender,
+                    :content => @message.content,
+                    :timestamp => @message.timestamp.to_js
                   }
+    else
+      render :json => @message.errors
     end
   end
   
--- app/models/chat/message.rb
+++ app/models/chat/message.rb
@@ -6,4 +6,31 @@ class Chat::Message
   field :timestamp, type: Time
   field :content, type: String
   
+  validates :timestamp, :presence => true
+  validates :sender, :presence => true
+  validates :content, :presence => true
+  
+  def content=(msg)
+    msg.gsub! /<\/?[^>]*>/, ""
+    msg.gsub! /[\u000d\u0009\u000c\u0085\u2028\u2029\n]/, "<br/>"
+    msg.gsub! /<br\/><br\/>/,"<br/>"
+    msg.gsub! /^\s*$/, ""
+    
+    if (m = msg.match(/^(.*)/i))
+      msg = m[0].gsub /([a-z]{1,6}\:\/\/)([a-z0-9\.\,\-\_]*)(\/?[a-z0-9\.\,\-\_\/\?\&\=\#]*)/i do
+        protocol = $1
+        top_dom = $2
+        path = $3
+        url = protocol + top_dom + path
+        "<a class='chat-link' href='#{url}' target='_blank'>#{url}</a>"
+      end
+    end
+    
+    self[:content] = msg
+  end
+  
+  before_validation do
+    self[:timestamp] = Time.now
+  end
+  
 end
