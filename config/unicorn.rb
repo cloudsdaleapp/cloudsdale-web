ba6c1e70ab9ed39ade523ef562ff97a0ae08a2a2
Philip Vieira <philip@vallin.se>
2011-10-18 04:41:12 +0200

    fix?

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,45 @@
-# <html>
-# <head><title>301 Moved Permanently</title></head>
-# <body bgcolor="white">
-# <center><h1>301 Moved Permanently</h1></center>
-# <hr><center>nginx/1.0.4</center>
-# </body>
-# </html>
+# Sample verbose configuration file for Unicorn (not Rack)
+#
+# This configuration file documents many features of Unicorn
+# that may not be needed for some applications. See
+# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
+# for a much simpler configuration file.
+#
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
 
-APP_PATH = "/opt/app/current"
-working_directory APP_PATH
+# Use at least one worker per core if you're on a dedicated server,
+# more will usually help for _short_ waits on databases/caches.
+worker_processes 4
 
-stdeer_path APP_PATH + "/opt/logs/unicorn.stderr.log"
-stdout_path APP_PATH + "/opt/logs/unicorn.stderr.log"
+# Since Unicorn is never exposed to outside clients, it does not need to
+# run on the standard HTTP port (80), there is no reason to start Unicorn
+# as root unless it's from system init scripts.
+# If running the master process as root and the workers as an unprivileged
+# user, do this to switch euid/egid in the workers (also chowns logs):
+# user "unprivileged_user", "unprivileged_group"
 
-pid APP_PATH + "/opt/pids/unicorn.pid"
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+working_directory "/opt/app/current" # available in 0.94.0+
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/.sock", :backlog => 64
+listen 8080, :tcp_nopush => true
+
+user 'deploy', 'web'
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+pid "/opt/pids/unicorn.pid"
+
+stdeer_path "/opt/logs/unicorn.stderr.log"
+stdout_path "/opt/logs/unicorn.stderr.log"
+
+# combine REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+# preload_app true
+# GC.respond_to?(:copy_on_write_friendly=) and
+#   GC.copy_on_write_friendly = true
