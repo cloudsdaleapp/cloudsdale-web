b3730f0be6e597b8a22151bfbc9fbb66a1aa767b
Philip Vieira <philip@vallin.se>
2011-10-18 02:35:19 +0200

    Added some deployment shizzle

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,148 @@
-require 'capistrano_colors'
+# config/deploy.rb 
 require "bundler/capistrano"
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))  # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                                # Load RVM's capistrano plugin.
+require 'capistrano_colors'
 
-set :application, "cloudsdale.org"
-
-
-set :rvm_bin_path, "/usr/local/rvm/bin"
-set :rvm_ruby_string, '1.9.2'
-set :rvm_type, :system
+set :scm,             :git
+set :repository,      "git@github.com:IOMUSE/Cloudsdale.git"
+set :branch,          "origin/deploy"
+set :migrate_target,  :current
+set :ssh_options,     { :forward_agent => true }
+set :rails_env,       "production"
+set :deploy_to,       "/opt/app"
 set :normalize_asset_timestamps, false
 
-set :repository,  "git@github.com:Zeeraw/Cloudsdale.git"
+set :user,            "deploy"
+set :group,           "web"
+set :password,        "2bb780e839bcd43b152ad5c614f7820befcd2847976e9771f73472865ca70963"
+set :use_sudo,        false
 
-set :scm, :git
-default_run_options[:pty] = true
+role :web, "50.57.151.151"
+role :app, "50.57.151.151", :primary => true
+role :db,  "50.57.151.151", :primary => true
 
-set :user, 'root'
-set :password, 'CloudsdaleomU46Su5R'
-set :use_sudo, false
-set :deploy_to, '/opt/app'
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-           # Web01        #Web02
-role :web, "50.57.125.171"                  
-role :app, "50.57.125.171"                  
-role :db,  "50.57.125.171", :primary => true 
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+default_environment["RAILS_ENV"] = 'production'
 
-# If you are using Passenger mod_rails uncomment this:
+default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.2-p290/bin:/usr/local/rvm/gems/ruby-1.9.2-p290@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
+default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290"
+default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ruby-1.9.2-p290:/usr/local/rvm/gems/ruby-1.9.2-p290@global"
+default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
+
+default_run_options[:shell] = 'bash'
+
+after 'deploy:assets:precompile', 'deploy:assets:upload'
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
+  end
+  
+  namespace :assets do
+    desc "Deploy assets to Rackspace CloudFiles"
+    task :upload, :roles => :app, :except => { :no_release => true }, :only => { :primary => true } do
+      run "cd #{current_path} ; #{rake} RAILS_ENV=#{rails_env} SHARED_PATH=#{shared_path} assets:upload"
+    end
+  end
+
+  desc "Setup your git-based deployment app"
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
+  end
+
+  task :cold do
+    update
+    migrate
+  end
+
   task :update do
-    run "cd /opt/app;#{try_sudo} git pull"
-    run "cd /opt/app;#{try_sudo} bundle install"
-    run "cd /opt/app;#{try_sudo} rake db:migrate RAILS_ENV=production"
-    run "cd /opt/app;#{try_sudo} rake assets:precompile RAILS_ENV=production"
+    transaction do
+      update_code
+    end
   end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd /opt/app;#{try_sudo} touch tmp/restart.txt"
+
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
   end
+
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
+  end
+
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn.main.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn.main.pid`"
+  end  
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
+  end
+end
+
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
 end
--- lib/tasks/assets.rake
+++ lib/tasks/assets.rake
@@ -1,47 +1,88 @@
-desc "Compile all the assets named in config.assets.precompile"
-task :precompile do
-  # We need to do this dance because RAILS_GROUPS is used
-  # too early in the boot process and changing here is already too late.
-  if ENV["RAILS_GROUPS"].to_s.empty? || ENV["RAILS_ENV"].to_s.empty?
-    ENV["RAILS_GROUPS"] ||= "assets"
-    ENV["RAILS_ENV"]    ||= "production"
-    Kernel.exec $0, *ARGV
-  else
-    Rake::Task["environment"].invoke
+require 'cloudfiles'
+cf = CloudFiles::Connection.new(
+  :username => Cloudsdale.config['rackspace_cloudfiles']['username'],
+  :api_key => Cloudsdale.config['rackspace_cloudfiles']['api_key'],
+  :snet => true
+)
 
-    # Ensure that action view is loaded and the appropriate sprockets hooks get executed
-    ActionView::Base
+namespace :assets do
+  
+  desc "Upload assets to rackspace"
+  task :upload do
+    
+    # Set up CloudFiles and paths
+    container = cf.container('cloudsdale_production_assets')
+    shared_path = Pathname.new(ENV["SHARED_PATH"])
+    assets_path = shared_path.join("assets")
+    remote_files = container.objects
+    local_files = Dir.glob(assets_path.join('**', '*.*')).collect{ |file| file.slice!(assets_path.to_s + '/'); file }
+    
+    if remote_files.sort == local_files.sort
+      puts "Remote and local the same. Skipping..."
+    else
+      puts "Starting remote sync..."
+      # See if there are files existing localy but not remotely
+      (local_files - remote_files).each do |file|
+        object = container.create_object(file, false)
+        object.load_from_filename(assets_path.join(file))
+        puts "Uploaded #{file}"
+      end
 
-    # Always calculate digests and compile files
-    Rails.application.config.assets.digest = true
-    Rails.application.config.assets.compile = true
-
-    config = Rails.application.config
-    env    = Rails.application.assets
-    target = Pathname.new(File.join(Rails.public_path, config.assets.prefix))
-    manifest = {}
-    manifest_path = config.assets.manifest || target
-
-    config.assets.precompile.each do |path|
-      env.each_logical_path do |logical_path|
-        if path.is_a?(Regexp)
-          next unless path.match(logical_path)
-        else
-          next unless File.fnmatch(path.to_s, logical_path)
-        end
-
-        if asset = env.find_asset(logical_path)
-          manifest[logical_path] = asset.digest_path
-          filename = target.join(asset.digest_path)
-          mkdir_p filename.dirname
-          asset.write_to(filename)
-          asset.write_to("#{filename}.gz") if filename.to_s =~ /\.(css|js)$/
-        end
+      # See if there is any files that exists remotely but are removed localy
+      (remote_files - local_files).each do |file|
+        container.delete_object(file)
+        puts "Removed #{file}"
       end
     end
-
-    File.open("#{manifest_path}/manifest.yml", 'w') do |f|
-      YAML.dump(manifest, f)
-    end
+    
   end
+  
 end
+
+# desc "Compile all the assets named in config.assets.precompile"
+# task :precompile do
+#   # We need to do this dance because RAILS_GROUPS is used
+#   # too early in the boot process and changing here is already too late.
+#   if ENV["RAILS_GROUPS"].to_s.empty? || ENV["RAILS_ENV"].to_s.empty?
+#     ENV["RAILS_GROUPS"] ||= "assets"
+#     ENV["RAILS_ENV"]    ||= "production"
+#     Kernel.exec $0, *ARGV
+#   else
+#     Rake::Task["environment"].invoke
+# 
+#     # Ensure that action view is loaded and the appropriate sprockets hooks get executed
+#     ActionView::Base
+# 
+#     # Always calculate digests and compile files
+#     Rails.application.config.assets.digest = true
+#     Rails.application.config.assets.compile = true
+# 
+#     config = Rails.application.config
+#     env    = Rails.application.assets
+#     target = Pathname.new(File.join(Rails.public_path, config.assets.prefix))
+#     manifest = {}
+#     manifest_path = config.assets.manifest || target
+# 
+#     config.assets.precompile.each do |path|
+#       env.each_logical_path do |logical_path|
+#         if path.is_a?(Regexp)
+#           next unless path.match(logical_path)
+#         else
+#           next unless File.fnmatch(path.to_s, logical_path)
+#         end
+# 
+#         if asset = env.find_asset(logical_path)
+#           manifest[logical_path] = asset.digest_path
+#           filename = target.join(asset.digest_path)
+#           mkdir_p filename.dirname
+#           asset.write_to(filename)
+#           asset.write_to("#{filename}.gz") if filename.to_s =~ /\.(css|js)$/
+#         end
+#       end
+#     end
+# 
+#     File.open("#{manifest_path}/manifest.yml", 'w') do |f|
+#       YAML.dump(manifest, f)
+#     end
+#   end
+# end
