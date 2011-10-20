ead466bee7bd51ffc5de04fdac07b4e149e6ee1a
Philip Vieira <philip@vallin.se>
2011-10-20 09:11:09 +0200

    Changed unicorn server

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,9 +8,8 @@ worker_processes 4
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 listen "/tmp/cloudsdale.socket", :backlog => 256
-listen 8080, :backlog => 256
 
-# Preload app for more speed
+# Preload our app for more speed
 preload_app true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
@@ -28,12 +27,35 @@ if env == 'production'
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
   stdout_path "#{shared_path}/log/unicorn.stdout.log"
 end
+  
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "/opt/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
 
-# combine REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-# preload_app true
-# GC.respond_to?(:copy_on_write_friendly=) and
-#   GC.copy_on_write_friendly = true
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
