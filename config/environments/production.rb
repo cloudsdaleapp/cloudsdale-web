4faf8bba351bb9269e71baac6a7c34889b3f26a0
Philip Vieira <philip@vallin.se>
2011-10-20 09:03:04 +0200

    Wrong url to assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Cloudsdale::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://assets.cloudsdale.com"
+  config.action_controller.asset_host = "http://assets.cloudsdale.org"
   config.assets.prefix = "/"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
