20a7a4e79503713a9ce591e5bd87b39685d225db
Philip Vieira <philip@vallin.se>
2011-11-01 04:01:05 +0100

    Assets precompile error.
    Trying to fix it!
    
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Cloudsdale::Application.configure do
   config.action_controller.perform_caching = true
 
   # 
-  config.assets.precompile = %w(application.css.sass *.ttf *.png *.jpg .jpeg *.gif *.js *.bmp)
+  config.assets.precompile = %w(application.css *.ttf *.png *.jpg .jpeg *.gif *.js *.bmp)
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
