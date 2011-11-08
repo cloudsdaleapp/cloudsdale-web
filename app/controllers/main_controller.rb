0969a536ca0edca7a706ef53221afe44b78e9c30
Philip Vieira <philip@vallin.se>
2011-11-08 22:07:05 +0100

    Fixed bug with featured entry.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   
   def index
     if current_user
-      @feartued_entry = Entry.first
+      @feartued_entry = Entry.where(promoted: true).first
       @recent_entries = Entry.order_by(:created_at,:desc).limit(5)
     end
   end
