ede29e5cdffae9159aca9056f10e9e261919cc57
Philip Vieira <philip@vallin.se>
2011-11-09 00:12:14 +0100

    Edited recent entry query for front page.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   def index
     if current_user
       @feartued_entry = Entry.where(promoted: true).first
-      @recent_entries = Entry.order_by(:created_at,:desc).limit(5)
+      @recent_entries = Entry.where(published: true, hidden: false).order_by(:updated_at,:desc).limit(5)
     end
   end
 
