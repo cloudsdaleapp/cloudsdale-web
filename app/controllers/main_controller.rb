8d31c671b2ed54ececd9fbc89d37fc98208a08cd
Philip Vieira <philip@vallin.se>
2011-11-09 16:20:15 +0100

    Popular entries
    I implemented them, which makes me popular too.
    
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,7 @@ class MainController < ApplicationController
     if current_user
       @feartued_entry = Entry.where(promoted: true).first
       @recent_entries = Entry.where(published: true, hidden: false).order_by(:updated_at,:desc).limit(5)
+      @popular_entries = Entry.where(published: true, hidden: false, :updated_at.gt => 3.days.ago).order_by([:views,:desc],[:comments,:desc],[:updated_at,:desc]).limit(5)
     end
   end
 
