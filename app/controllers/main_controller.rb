717a963118bd74f4fdf9dbd35ce42f79eaa45b7e
Philip Vieira <philip@vallin.se>
2011-11-09 18:51:42 +0100

    Limited amount of entries on front page.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,8 +3,8 @@ class MainController < ApplicationController
   def index
     if current_user
       @feartued_entry = Entry.where(promoted: true).first
-      @recent_entries = Entry.where(published: true, hidden: false).order_by(:updated_at,:desc).limit(5)
-      @popular_entries = Entry.where(published: true, hidden: false, :updated_at.gt => 3.days.ago).order_by([:views,:desc],[:comments,:desc],[:updated_at,:desc]).limit(5)
+      @recent_entries = Entry.where(published: true, hidden: false).order_by(:updated_at,:desc).limit(3)
+      @popular_entries = Entry.where(published: true, hidden: false, :updated_at.gt => 3.days.ago).order_by([:views,:desc],[:comments,:desc],[:updated_at,:desc]).limit(3)
     end
   end
 
