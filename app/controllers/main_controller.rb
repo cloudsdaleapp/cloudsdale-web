e38fba8b4bb49d7a774c881f7ff444cdf735be0b
Philip Vieira <philip@vallin.se>
2011-11-10 16:44:48 +0100

    Added Publish timestamp to Entry
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,8 +8,8 @@ class MainController < ApplicationController
   
   def index
     @feartued_entry = Entry.where(promoted: true).first
-    @recent_entries = Entry.where(published: true, hidden: false, promoted: false).order_by(:updated_at,:desc).limit(3)
-    @popular_entries = Entry.where(published: true, hidden: false, promoted: false, :updated_at.gt => 3.days.ago).order_by([:views,:desc],[:comments,:desc],[:updated_at,:desc]).limit(3)
+    @recent_entries = Entry.where(published: true, hidden: false, promoted: false).order_by(:published_at,:desc).limit(3)
+    @popular_entries = Entry.where(published: true, hidden: false, promoted: false, :published_at.gt => 3.days.ago).order_by([:views,:desc],[:comments,:desc],[:published_at,:desc]).limit(3)
   end
 
 end
