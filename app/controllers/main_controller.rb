ea162c6f4079f3bfb6f096de492b139e7b8888d5
Philip Vieira <philip@vallin.se>
2011-12-08 13:25:12 +0100

    Cleaned up a controller for some reason...
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,14 +13,16 @@ class MainController < ApplicationController
     @featured_entry = Entry.where(promoted: true).first
     @popular_clouds = Cloud.where(hidden: false).order_by([:member_count,:desc]).limit(6)
     @recent_clouds = Cloud.where(hidden: false).order_by([:created_at,:desc]).limit(6)
+
     case params[:tab]
       when nil
         @entries = Entry.where(published: true, hidden: false, promoted: false).order_by(:published_at,:desc).limit(10)
       when "subscriptions"
-        @entries = Entry.any_in(author_id:current_user.publisher_ids).order_by([:published_at,:desc]).limit(10)
+        @entries = Entry.any_in(author_id: current_user.publisher_ids).order_by([:published_at,:desc]).limit(10)
       when "popular"
         @entries = Entry.where(published: true, hidden: false, promoted: false, :published_at.gt => 7.days.ago).order_by([:views,:desc],[:comments,:desc],[:published_at,:desc]).limit(10)
     end
+
   end
 
 end
