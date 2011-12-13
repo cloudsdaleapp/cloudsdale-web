b47c0e4469c124258b8a3f3fb5bd9850f6e23950
Micke Lisinge <lisinge@gmail.com>
2011-12-13 16:44:35 +0100

    Use mongo to sort messages instead of ruby

--- app/controllers/chat/rooms/messages_controller.rb
+++ app/controllers/chat/rooms/messages_controller.rb
@@ -9,7 +9,7 @@ class Chat::Rooms::MessagesController < ApplicationController
   end
   
   def index
-    @messages = @room.messages.order_by([:timestamp,:desc]).limit(50).reverse
+    @messages = @room.messages.order_by([:timestamp,:asc]).limit(50)
     respond_to do |format|
       format.json { render json: @messages.to_json }
     end
