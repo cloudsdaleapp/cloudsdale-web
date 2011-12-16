4377017a3a639c3dae90902bdf109b8444e19438
Philip Vieira <philip@vallin.se>
2011-12-16 18:28:43 +0100

    Now properly provides search results with arrays of results.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,12 +5,20 @@ class SearchController < ApplicationController
     
     @query = params[:q]
     
-    @clouds = Cloud.tire.search(@query)
-    @entries = []
-    @users = User.tire.search(@query)
+    @clouds = Tire.search('clouds') do |s|
+      s.query { |q| q.string @query}
+    end
+    
+    @entries = Tire.search(['articles']) do |s|
+      s.query { |q| q.string @query}
+    end
+
+    @users = Tire.search('users') do |s|
+      s.query { |q| q.string @query}
+    end
 
     respond_to do |format|
-      format.js { render json: { clouds: @clouds, entries: @entries, users: @users} }
+      format.js { render json: { clouds: @clouds.results[0..2], entries: @entries.results[0..2], users: @users.results[0..2]} }
     end
     
   end
