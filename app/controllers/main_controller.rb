009486e8be5f9c8b01a627fff53b0b0b1585f021
Philip Vieira <philip@vallin.se>
2012-01-06 14:43:15 +0100

    Fixed login bug
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,15 @@
 class MainController < ApplicationController
   
+  before_filter only: [:index] do
+    unless current_user
+      redirect_to login_path
+    end
+  end
+  
   def index
 
     @drops = Drop.order_by(:updated_at,:desc).limit(20)
+    
     
   end
 
