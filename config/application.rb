b55c4268dc5d68792ec9279c31bd0b672b5531e2
Philip Vieira <philip@vallin.se>
2012-01-15 21:03:37 +0100

    Prepared faye path references to work in production mode
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,9 @@ module Cloudsdale
     @rails_config ||= YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   end
   
-  def self.faye_path
-    @faye_path = URI.parse("http://#{Cloudsdale.config['faye']['host']}:#{Cloudsdale.config['faye']['port']}/faye")
+  def self.faye_path(connection=nil)
+    host = (connection == :inhouse) ? config['faye']['inhouse_host'] : config['faye']['host']
+    @faye_path = URI.parse("#{config['faye']['schema']}://#{host}:#{config['faye']['port']}/#{config['faye']['path']}")
   end
   
   class Application < Rails::Application
