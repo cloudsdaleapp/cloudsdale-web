864ec921ea8d030a18386d446d63582735fde87f
Philip Vieira <philip@vallin.se>
2012-01-19 02:06:59 +0100

    Fixed severe deposit bug
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -10,6 +10,8 @@ class Deposit
   before_save do
     self["#{depositable_id.to_s}_updated_at".to_sym] = !self.updated_at.nil? ? self.updated_at.utc : Time.now.utc
     self["#{depositable_id.to_s}_created_at".to_sym] = !self.created_at.nil? ? self.created_at.utc : Time.now.utc
+    self[:depositable_type] = self[:depositable_type].classify
+    self[:depositer_ids].uniq!
   end
 
 end
--- config/initializers/drop/droppable.rb
+++ config/initializers/drop/droppable.rb
@@ -3,7 +3,7 @@ module Droppable
   def create_drop_deposit_from_url_by_user(url,user)
     drop = Drop.find_or_initialize_from_matched_url(url)
     if drop.save
-      deposit = drop.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type)
+      deposit = drop.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type.classify)
       deposit.depositers << user unless deposit.depositer_ids.include?(user.id)
       deposit.save
       drop
