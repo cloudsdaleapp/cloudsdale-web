82a6eb260fa2d80894098b2be472951e7aae17a2
Philip Vieira <philip@vallin.se>
2012-01-22 21:10:11 +0100

    Maybe fixes the problem
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/uploaders/preview_uploader.rb
+++ app/uploaders/preview_uploader.rb
@@ -6,6 +6,10 @@ class PreviewUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
+  
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
