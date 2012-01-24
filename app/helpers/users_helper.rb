aa5a2ec0a092b782b9339e23b0372e7df8abaa80
Philip Vieira <philip@vallin.se>
2012-01-24 10:11:28 +0100

    Incorporated helper to show proper user image.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,14 @@ module UsersHelper
     (user == current_user) ? "You" : user.name
   end
   
+  def user_avatar_path(path,version=nil)
+    if !path.nil?
+      path
+    elsif !version.nil?
+      image_path("other/unknown_pony_#{version.to_s}.jpg")
+    else
+      image_path("other/unknown_pony.jpg")
+    end
+  end
+  
 end
