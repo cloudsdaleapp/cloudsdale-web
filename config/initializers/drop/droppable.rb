28acecfb81e85ed422d5b45bfb56e0314d76973a
Philip Vieira <philip@vallin.se>
2012-01-24 12:31:11 +0100

    Fuckup
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/initializers/drop/droppable.rb
+++ config/initializers/drop/droppable.rb
@@ -3,7 +3,7 @@ module Droppable
   def create_drop_deposit_from_url_by_user(url,user)
     d = Drop.find_or_initialize_from_matched_url(url)
     if d.save
-      dp = drop.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type.classify)
+      dp = d.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type.classify)
       dp.depositers << user unless dp.depositer_ids.include?(user.id)
       dp.save
       d
