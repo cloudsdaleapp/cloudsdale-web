01043cb8bf5b92afa6dddedaa6a7ba5fd523691f
Philip Vieira <philip@vallin.se>
2012-01-24 12:29:32 +0100

    Fixed bug
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/initializers/drop/droppable.rb
+++ config/initializers/drop/droppable.rb
@@ -1,12 +1,12 @@
 module Droppable
   
   def create_drop_deposit_from_url_by_user(url,user)
-    drop = Drop.find_or_initialize_from_matched_url(url)
-    if drop.save
-      deposit = drop.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type.classify)
-      deposit.depositers << user unless deposit.depositer_ids.include?(user.id)
-      deposit.save
-      drop
+    d = Drop.find_or_initialize_from_matched_url(url)
+    if d.save
+      dp = drop.deposits.find_or_initialize_by(depositable_id: self.id, depositable_type: self._type.classify)
+      dp.depositers << user unless dp.depositer_ids.include?(user.id)
+      dp.save
+      d
     else
       nil
     end
