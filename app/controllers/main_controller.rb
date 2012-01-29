3572e375f14ce4d8611a09cf8af3c839a2207175
Philip Vieira <philip@vallin.se>
2012-01-29 05:58:22 +0100

    Fix, maybe
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,7 +12,7 @@ class MainController < ApplicationController
     @depositable_ids = (current_user.publisher_ids + [current_user.id]).uniq + current_user.cloud_ids
     @drops = Drop.any_of(:"deposits.depositable_id".in => @depositable_ids).order_by('deposits.updated_at',:desc).page(params[:page] || 1).per(10)
     @suggested_clouds = Cloud.where(hidden: false, :user_ids.nin => [current_user.id]).order_by(:member_count,:desc).limit(10)
-    @popular_users = User.where(invisible: false, :subscriber_ids.nin => [current_user.id]).order_by(:subscribers_count,:desc).limit(10)
+    @popular_users = User.where(invisible: false, :subscriber_ids.nin => current_user.publisher_ids + [current_user.id]).order_by(:subscribers_count,:desc).limit(10)
     respond_to do |format|
       format.html {  }
       format.js { render partial: 'drops/list_content' }
