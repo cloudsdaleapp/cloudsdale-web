0554bf2e3791db1c383cf08220f502d239c69ff1
Philip Vieira <philip@vallin.se>
2012-02-02 20:42:45 +0100

    Added helper for drop timestamps.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- app/helpers/drops_helper.rb
+++ app/helpers/drops_helper.rb
@@ -10,4 +10,16 @@ module DropsHelper
     return "#"
   end
   
+  def drop_timestamp(drop,deposits,depositable=nil)
+    if depositable.nil?
+      ts = time_ago_in_words(drop.updated_at)
+    else
+      latest_update = deposits.order_by("#{depositable.id.to_s}_updated_at",:desc).first.try(:updated_at) || Time.now.utc
+      ts = time_ago_in_words(latest_update)
+    end
+    content_tag(:span, class: :timestamp) do
+      "#{ts} ago"
+    end
+  end
+  
 end
