14afe5598406824cdfd7076504b82cd663060133
Philip Vieira <philip@vallin.se>
2012-02-13 03:17:58 +0100

    Added admin GUI
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,10 @@ Cloudsdale::Application.routes.draw do
   
   resources :search, only: [:create]
   
-  resources :admin, only: [:index]
+  resources :admin, only: [:index] do
+    collection do
+      post :statistics
+    end
+  end
   
 end
