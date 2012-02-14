885ea20092d7bc5a10fa34148f357897e9eb2517
Philip Vieira <philip@vallin.se>
2012-02-14 18:04:26 +0100

    Changed secret token to reset all user sessions.
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cloudsdale::Application.config.secret_token = 'a7990f4da5046a53b7abf5e31eaaac36320ccc4fa0df24ba0f39e42a6d117c0b40b80547b40a7881e53f9ba47af955454bb9138bbd01b1594db3f724dd830e2f'
+Cloudsdale::Application.config.secret_token = 'b7990f4da5046a53b7abf5e31eaaac36320ccc4fa0df24ba0f39e42a6d117c0b40b80547b40a7881e53f9ba47af955454bb9138bbd01b1594db3f724dd830e2f'
