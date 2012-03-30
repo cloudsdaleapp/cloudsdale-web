29a3ac5b9fe08006bd3251313d427b7823e9edfb
Philip Vieira <philip@vallin.se>
2012-03-30 22:19:10 +0200

    Fixed routing_error to render the exception layout.

--- app/controllers/api/v1/exceptions_controller.rb
+++ app/controllers/api/v1/exceptions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ExceptionsController < Api::V1Controller
   
   def routing_error
     @error = "Could not resolve the path - head over to http://developer.cloudsdale.org/ for more information"
-    render status: 400
+    render :exception, status: 400
   end
   
 end
