1d4a7cae2747fd56940aa8ac8d93096e6a813025
Philip Vieira <philip@vallin.se>
2012-03-31 00:40:11 +0200

    Gave the chat a method to determine the last chat event timestamp.

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,6 +5,14 @@ class Chat
   embedded_in :topic, polymorphic: true
   embeds_many :messages
   
-  field :token,     type: String
+  field :token,         type: String
+  
+  # Public: Fetches the time for when the last message
+  # was sent over this chat
+  #
+  # Returns a DateTime object.
+  def last_message_at
+    messages.order_by([:timestamp,:desc]).limit(1).first.timestamp
+  end
   
 end
