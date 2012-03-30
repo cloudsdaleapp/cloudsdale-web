1cadf9da76fc48eef4ae2523ce5f019ebac3fbd7
Philip Vieira <philip@vallin.se>
2012-03-30 16:37:13 +0200

    Implemented redis sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-Cloudsdale::Application.config.session_store :mongo_store, key: '_cloudsdale_session', collection: lambda { Mongoid.master.collection('sessions') }
+# Cloudsdale::Application.config.session_store :mongo_store, key: '_cloudsdale_session', collection: lambda { Mongoid.master.collection('sessions') }
+
+Cloudsdale::Application.config.session_store :redis_store,
+  :servers => {
+    :host => Cloudsdale.config['redis']['host'],
+    :port => Cloudsdale.config['redis']['port'],
+    :db => 0,
+    :namespace => 'cloudsdale:sessions'
+  }
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
