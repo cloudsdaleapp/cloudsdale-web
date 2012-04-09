0822380a7909a9a5411e7c0ec5837bd13be716f3
Philip Vieira <philip@vallin.se>
2012-04-09 23:34:26 +0200

    Removed HTML-fying of links in messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -47,21 +47,10 @@ class Message
   end
   
   def content=(msg)
-    msg.gsub! /<\/?[^>]*>/, ""
-    msg.gsub! /[\u000d\u0009\u000c\u0085\u2028\u2029\n]/, "<br/>"
-    msg.gsub! /<br\/><br\/>/,"<br/>"
-    msg.gsub! /^\s*$/, ""
-    
     if (m = msg.match(/^(.*)/i))
       msg = m[0].gsub /([a-z]{1,6}\:\/\/)([a-z0-9\.\,\-\_\:]*)(\/?[a-z0-9\!\'\"\.\,\-\_\/\?\:\&\=\#\%\+\(\)]*)/i do
-        protocol = $1
-        top_dom = $2
-        path = $3
-        pjax_enabled = !(top_dom =~ /cloudsdale.org/i).nil?
-        url = protocol + top_dom + path
         self[:urls] ||= []
         self[:urls] << url
-        "<a class='chat-link' href='#{url}' #{!pjax_enabled ? "target='_blank' data-skip-pjax='true'" : ''}>#{url}</a>"
       end
     end
     
