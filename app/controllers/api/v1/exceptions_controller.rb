df084a553dac0b0562e2bbfd85c1bedfe80f4a88
Philip Vieira <philip@vallin.se>
2012-05-13 18:20:47 +0200

    Added documentation to Api::V1::ExceptionsController#routing_error

--- app/controllers/api/v1/exceptions_controller.rb
+++ app/controllers/api/v1/exceptions_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ExceptionsController < Api::V1Controller
   
+  # Internal: The path to which you get directed when no routes are matched.
+  # Using the api.
+  #
+  # Returns a 400 error together with an error message.
   def routing_error
     
     add_error message: "Could not resolve the path - head over to http://developer.cloudsdale.org/ for more information"
