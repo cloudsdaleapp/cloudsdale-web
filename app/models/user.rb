c951a5f14d8b16a98e2b037a18df553db0e926dd
Philip Vieira <philip@vallin.se>
2012-05-16 15:43:03 +0200

    Added case insensitive uniqueness validators to User.rb :name & :email

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,9 +40,12 @@ class User
   accepts_nested_attributes_for :authentications, :allow_destroy => true
   
   validates :name,        length: { within: 2..20 }, format: { :with => /^[a-z0-9\_]*$/i }, allow_blank: true
-  validates :email,       format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, uniqueness: true, allow_blank: true
+  validates :email,       format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, allow_blank: true
   validates :password,    length: { within: 6..56 }, allow_blank: true
   validates :auth_token,  uniqueness: true, allow_blank: true
+  
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
   
   validates_presence_of [:email,:password,:name], :if => :confirm_registration
     
