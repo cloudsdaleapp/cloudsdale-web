ad908583c5475c49cd6a1b8c53366d3d81370d98
Philip Vieira <philip@vallin.se>
2012-05-28 19:40:32 +0200

    Now creates drops before a message is saved.

--- app/controllers/api/v1/clouds/chat/messages_controller.rb
+++ app/controllers/api/v1/clouds/chat/messages_controller.rb
@@ -34,7 +34,11 @@ class Api::V1::Clouds::Chat::MessagesController < Api::V1Controller
     @message.author = current_user
     
     authorize! :create, @message
-      
+    
+    @message.urls.each do |url|
+      @cloud.create_drop_deposit_from_url_by_user(url,current_user)
+    end
+
     if @message.save
       render status: 200
     else
