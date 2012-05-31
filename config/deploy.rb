f597c3a46434b9f274c9513770ebd69278ca094f
Philip Vieira <philip@vallin.se>
2012-05-31 23:11:12 +0200

    Selected the correct branch for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require 'capistrano_colors'
 
 set :scm,             :git
 set :repository,      "git@github.com:IOMUSE/Cloudsdale.git"
-set :branch,          "origin/deploy"
+set :branch,          "origin/server-migration-04"
 set :migrate_target,  :current
 set :ssh_options,     { :forward_agent => true }
 set :rails_env,       "production"
