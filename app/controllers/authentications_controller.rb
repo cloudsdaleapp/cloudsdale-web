19bc2af00971198488407fa9a4b299c7afde97aa
Philip Vieira <philip@vallin.se>
2012-06-10 01:27:08 +0200

    Tried to fix authentication issue.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -22,12 +22,12 @@ class AuthenticationsController < ApplicationController
     @name   = omniauth.info.nickname
     @email  = omniauth.info.email
     
-    if omniauth.info.location
-      ActiveSupport::TimeZone.zones_map.keys.each do |allowed_time_zone|
-        @time_zone = allowed_time_zone if omniauth.info.location.match(/#{allowed_time_zone}/i)
-      end
-    end
-    
+    # if omniauth.info.location
+    #   ActiveSupport::TimeZone.zones_map.keys.each do |allowed_time_zone|
+    #     @time_zone = allowed_time_zone if omniauth.info.location.match(/#{allowed_time_zone}/i)
+    #   end
+    # end
+    # 
     @user = User.where('authentications.provider' => @provider, 'authentications.uid' => @uid).first
     
     if @user
@@ -44,14 +44,17 @@ class AuthenticationsController < ApplicationController
         @user = current_user
       end
       
-      @user.authentications.build(provider: @provider, uid: @uid)
+      @user.authentications.find_or_initialize_by(provider: @provider, uid: @uid)
+      @user.save
     end
         
     @user.name      = @name       unless @user.name.present? or (User.where(name: /^#{@name}$/).count >= 1)
     @user.email     = @email      unless @user.email.present?
-    @user.time_zone = @time_zone  unless @user.time_zone.present?
+    # @user.time_zone = @time_zone  unless @user.time_zone.present?
     
-    if !@user.banned? && @user.save
+    @user.save
+    
+    if !@user.banned?
       session[:user_id] = @user.id
     end
     
