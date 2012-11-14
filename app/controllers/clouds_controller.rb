36c07943b5887e23532df4f0fcbe69819101a37e
Philip Vieira <philip@vallin.se>
2012-11-14 17:07:22 +0100

    Set some instance variables in Clouds controller

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,8 +1,17 @@
 class CloudsController < ApplicationController
-  
+
   def show
     @cloud = Cloud.find(params[:id])
+
+    @page_title = "Cloudsdale - #{@cloud.name}"
+    @page_image = @cloud.avatar.url
+    @page_description = @cloud.description
+    @page_type = "group"
+    @page_url = cloud_url(@cloud.id)
+
     authorize! :read, @cloud
+    render status: 200
+
   end
-  
+
 end
