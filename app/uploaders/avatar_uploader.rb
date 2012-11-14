3f298b1df47384119f45d8a203715a04c6ed46c4
Philip Vieira <philip@vallin.se>
2012-11-14 17:06:51 +0100

    Removed trailing whitespace in avatar uploader.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
-  
+
   include CarrierWave::MiniMagick
   include Sprockets::Helpers::RailsHelper
-  
+
   def config
     Rails.application.config.action_controller
   end
@@ -12,10 +12,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def controller
     nil
   end
-  
+
   # Choose what kind of storage to use for this uploader:
   storage :fog
-  
+
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
@@ -25,18 +25,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "avatars/#{model.id}/"
   end
-  
+
   # Process files as they are uploaded:
   process :resize_to_fill => [200, 200]
   process :convert => 'png'
 
   # Create different versions of your uploaded files:
-  
+
   version :mini do
     process :resize_to_fill => [24, 24]
     process :convert => 'png'
   end
-  
+
   version :thumb do
     process :resize_to_fill => [50, 50]
     process :convert => 'png'
@@ -51,26 +51,26 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [70, 70]
     process :convert => 'png'
   end
-  
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-  
+
   def filename
      "#{secure_token(10)}-avatar.png" if original_filename.present?
   end
-  
+
   def default_url
-    image_path("#{Cloudsdale.config['asset_url']}/assets/fallback/" + [mounted_as, version_name, "#{model.class.to_s.downcase}.png"].compact.join('_'))    
+    image_path("#{Cloudsdale.config['asset_url']}/assets/fallback/" + [mounted_as, version_name, "#{model.class.to_s.downcase}.png"].compact.join('_'))
   end
 
   protected
-  
+
   def secure_token(length=16)
     var = :"@#{mounted_as}_secure_token"
     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
-  
+
 end
