d3e6673cddf46754d45b10332293ed430b2a67ff
Philip Vieira <philip@vallin.se>
2012-11-17 20:35:10 +0100

    Fixed up abilities a little.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,31 +13,31 @@ class Ability
 
     if user.is_of_role?(:normal)
 
-      unless user.banned?
+      can [:accept_tnc,:update], User, _id: user.id
 
-        can [:accept_tnc,:update], User, _id: user.id
+      if user.is_registered? and not user.banned?
 
         # Normal users can create clouds.
-        can :create, Cloud do
-          user.is_registered?
-        end
+        can :create, Cloud
 
         # Normal users can destroy clouds they own.
         can [:update,:destroy], Cloud, :owner_id => user.id
 
         # Normal users can add clouds if they are registered.
         can :add, Cloud do |cloud|
-          user.is_registered?
+          banned_from_cloud = cloud.has_banned?(user)
+          not banned_from_cloud
         end
 
         # Normal users can deduct clouds if their cloud portfolio include that cloud.
-        can :deduct, Cloud do |cloud|
-          user.is_registered?
-        end
+        can :deduct, Cloud
 
         # Normal users can only create messages in clouds that are unlocked or that they have added.
         can :create, Message do |message|
-          (user.cloud_ids.include?(message.chat.topic.id) || !message.chat.topic.locked?) && user.is_registered?
+          i_can_communicate_in_topic  = user.cloud_ids.include?(message.chat.topic.id)
+          topic_is_not_locked         = !message.chat.topic.locked?
+
+          i_can_communicate_in_topic or topic_is_not_locked
         end
 
         can [:create,:update], Ban do |ban|
@@ -45,7 +45,7 @@ class Ability
           prosecuting_myself      = ban.offender.id == user.id
           offender_was_there      = ban.jurisdiction.user_ids.include?(offender.id)
 
-          i_am_a_moderator and offender_was_there and !prosecuting_myself
+          i_am_a_moderator and offender_was_there and not prosecuting_myself
         end
 
       end
@@ -77,7 +77,6 @@ class Ability
 
     if user.is_of_role?(:admin)
       can :destroy, Cloud
-      can :destroy, User
       can :create, Message
     end
 
