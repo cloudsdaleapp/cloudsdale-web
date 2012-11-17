3ae4b2563fc8dd866c8b106a72959ceee4c6a338
Philip Vieira <philip@vallin.se>
2012-11-17 20:12:55 +0100

    Cleaned some whitespace in AMQPConnector

--- config/initializers/amqp_connector.rb
+++ config/initializers/amqp_connector.rb
@@ -1,16 +1,16 @@
 module AMQPConnector
-    
+
   def enqueue!(queue,data)
-    
+
     encoded_data = data.class == String ? data : Yajl::Encoder.encode(data)
-        
+
     q = Cloudsdale.bunny.queue(queue)
     e = Cloudsdale.bunny.exchange('')
-    
+
     e.publish(encoded_data, :key => queue.to_s, :content_type => "application/json")
-    
+
   end
-  
+
 end
 
 Cloudsdale.bunny.queue 'faye'
