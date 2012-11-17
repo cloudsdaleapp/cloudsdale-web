eba89c86dd6c39ae21f5a930330eae27e22579e0
Philip Vieira <philip@vallin.se>
2012-11-17 09:52:00 +0100

    Patched Time.rb even further.

--- config/initializers/patches/time.rb
+++ config/initializers/patches/time.rb
@@ -6,6 +6,34 @@ module ActiveSupport
       strftime("%Y/%m/%d %H:%M:%S %z")
     end
 
+    def to_ms
+      (self.to_f * 1000.0).to_i
+    end
+
+  end
+
+end
+
+class Time
+
+  def to_ms
+    (self.to_f * 1000.0).to_i
+  end
+
+  end
+
+class Date
+
+  def to_ms
+    (self.to_f * 1000.0).to_i
+  end
+
+end
+
+class DateTime
+
+  def to_ms
+    (self.to_f * 1000.0).to_i
   end
 
 end
