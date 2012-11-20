dce9f95ea4a3e76c14ba9ade6098206eaffd2d15
Philip Vieira <philip@vallin.se>
2012-11-20 04:22:30 +0100

    Removed unnecessary line in ban.rb

--- app/models/ban.rb
+++ app/models/ban.rb
@@ -19,7 +19,6 @@ class Ban
   attr_accessible :reason,  as: [:enforcer]
 
   validates :reason, length: { within: 1..140 }
-  validates :due
   validate :validate_due
 
   scope :active, where(:due.gt => DateTime.current, :revoke => false)
