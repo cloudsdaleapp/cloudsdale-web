e972b8a002abba961596009b81fa08d5db4ae656
Philip Vieira <philip@vallin.se>
2012-11-22 15:47:50 +0100

    Added a shaitload of tags.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,74 +11,62 @@ class Ability
       !u.banned?
     end
 
-    if user.is_of_role?(:normal)
+    can [:accept_tnc,:update], User, _id: user.id
 
-      can [:accept_tnc,:update], User, _id: user.id
+    if user.is_registered? and not user.banned?
 
-      if user.is_registered? and not user.banned?
+      # Normal users can create clouds.
+      can :create, Cloud
 
-        # Normal users can create clouds.
-        can :create, Cloud
+      # Normal users can destroy clouds they own.
+      can [:update,:destroy], Cloud, :owner_id => user.id
 
-        # Normal users can destroy clouds they own.
-        can [:update,:destroy], Cloud, :owner_id => user.id
+      # Normal users can add clouds if they are registered.
+      can :add, Cloud do |cloud|
+        banned_from_cloud = cloud.has_banned?(user)
+        not banned_from_cloud
+      end
 
-        # Normal users can add clouds if they are registered.
-        can :add, Cloud do |cloud|
-          banned_from_cloud = cloud.has_banned?(user)
-          not banned_from_cloud
-        end
+      # Normal users can deduct clouds if their cloud portfolio include that cloud.
+      can :deduct, Cloud
 
-        # Normal users can deduct clouds if their cloud portfolio include that cloud.
-        can :deduct, Cloud
+      # Normal users can only create messages in clouds that are unlocked or that they have added.
+      can :create, Message do |message|
+        i_can_communicate_in_topic  = user.cloud_ids.include?(message.chat.topic.id)
+        topic_is_not_locked         = !message.chat.topic.locked?
+        i_am_not_banned_from_topic  = !message.chat.topic.has_banned?(user)
 
-        # Normal users can only create messages in clouds that are unlocked or that they have added.
-        can :create, Message do |message|
-          i_can_communicate_in_topic  = user.cloud_ids.include?(message.chat.topic.id)
-          topic_is_not_locked         = !message.chat.topic.locked?
-          i_am_not_banned_from_topic  = !message.chat.topic.has_banned?(user)
+        (i_can_communicate_in_topic || topic_is_not_locked) && i_am_not_banned_from_topic
+      end
 
-          (i_can_communicate_in_topic || topic_is_not_locked) && i_am_not_banned_from_topic
-        end
+      can [:list], Ban do |ban|
+        ban.jurisdiction.moderator_ids.include?(user.id)
+      end
 
-        can [:list], Ban do |ban|
-          ban.jurisdiction.moderator_ids.include?(user.id)
-        end
+      can [:create,:update], Ban do |ban|
+        i_am_a_moderator        = ban.jurisdiction.moderator_ids.include?(user.id)
+        not_prosecuting_myself  = ban.offender.id != user.id
+        offender_was_there      = ban.jurisdiction.user_ids.include?(ban.offender.id)
 
-        can [:create,:update], Ban do |ban|
-          i_am_a_moderator        = ban.jurisdiction.moderator_ids.include?(user.id)
-          not_prosecuting_myself  = ban.offender.id != user.id
-          offender_was_there      = ban.jurisdiction.user_ids.include?(ban.offender.id)
-
-          (i_am_a_moderator || offender_was_there) && not_prosecuting_myself
-        end
-
+        (i_am_a_moderator || offender_was_there) && not_prosecuting_myself
       end
 
     end
 
-    if user.is_of_role?(:donor)
-      # ...
-    end
+    # if user.is_of_role?(:moderator)
 
-    if user.is_of_role?(:moderator)
+    #   if !user.banned?
 
-      if !user.banned?
+    #     # You can't ban users that have higher role than you
+    #     can [:ban,:unban], User do |_user|
+    #       (_user.try(:_id) != user.try(:_id)) && user.role > _user.role
+    #     end
 
-        # You can't ban users that have higher role than you
-        can [:ban,:unban], User do |_user|
-          (_user.try(:_id) != user.try(:_id)) && user.role > _user.role
-        end
+    #     can :destroy, Drop
 
-        can :destroy, Drop
+    #   end
 
-      end
-
-    end
-
-    if user.is_of_role? :placeholder
-      # ...
-    end
+    # end
 
     if user.is_of_role?(:admin)
       can :destroy, Cloud
