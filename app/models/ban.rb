875bdc974f14e1990552dea95dfe08bcda2e30aa
Philip Vieira <philip@vallin.se>
2012-11-23 19:38:18 +0100

    Only validate due if due changed.

--- app/models/ban.rb
+++ app/models/ban.rb
@@ -19,7 +19,7 @@ class Ban
   attr_accessible :reason,  as: [:enforcer]
 
   validates :reason, length: { within: 1..140 }
-  validate :validate_due
+  validate :validate_due, if: :due_changed?
 
   scope :active, where(:due.gt => DateTime.current, :revoke => false)
   scope :revoked, where(revoke: true)
