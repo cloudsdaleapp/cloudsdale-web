065bd75d0d906c005ec61cca36df31a8ee1a8784
Philip Vieira <philip@vallin.se>
2012-11-28 18:04:37 +0100

    Updated sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,21 +1,21 @@
 class SitemapController < ApplicationController
-  
+
   layout nil
 
   def index
-    
+
     headers['Content-Type'] = 'application/xml'
-    
+
     last_cloud = Cloud.last
-    
+
     if stale?(:etag => last_cloud, :last_modified => last_cloud.updated_at.utc)
-      
+
       respond_to do |format|
-        format.xml { @clouds = Cloud.order_by([:member_count,:desc]).only([:id,:updated_at]) } # sitemap is a named scope
+        format.xml { @clouds = Cloud.order_by([:member_count,:desc]).only([:id,:updated_at,:short_name]) } # sitemap is a named scope
       end
-      
+
     end
-    
+
   end
-  
+
 end
