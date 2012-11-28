88e243f2054fb9b8fd3a7a1a2007de328dfdb106
Philip Vieira <philip@vallin.se>
2012-11-28 17:32:26 +0100

    Updated facebooks auth request hash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Cloudsdale.config['facebook']['app_key'].to_s, Cloudsdale.config['facebook']['app_secret'].to_s
+  provider :facebook, Cloudsdale.config['facebook']['app_key'].to_s, Cloudsdale.config['facebook']['app_secret'].to_s, scope: 'email,publish_stream'
   provider :twitter, Cloudsdale.config['twitter']['consumer_key'].to_s, Cloudsdale.config['twitter']['consumer_secret'].to_s
 end
