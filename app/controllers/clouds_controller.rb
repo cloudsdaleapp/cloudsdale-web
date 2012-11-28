875133a6c52e7eec5210d1f0f8435e9fd42aa4fb
Philip Vieira <philip@vallin.se>
2012-11-28 14:36:53 +0100

    Fixed some open graph bugs.

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,14 +1,15 @@
 class CloudsController < ApplicationController
 
   def show
-    # @cloud = Cloud.find(params[:id_or_short_name]) or Cloud.where( short_name: params[:id_or_short_name]).first
+
     id_or_short_name = params[:id_or_short_name]
     @cloud = Cloud.where("$or" => [{id: id_or_short_name}, {short_name: id_or_short_name}]).first
+
     @page_title = "Cloudsdale - #{@cloud.name}"
     @page_image = @cloud.avatar.url
     @page_description = @cloud.description
-    @page_type = "group"
-    @page_url = cloud_url(@cloud.id)
+    @page_type = "cloudsdale:cloud"
+    @page_url = @cloud.short_name.present? cloud_url(@cloud.short_name) : cloud_url(@cloud.id)
 
     authorize! :read, @cloud
     render status: 200
