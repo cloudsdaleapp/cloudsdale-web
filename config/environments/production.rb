27b94433a02b688c7b6504cd5406e61322ec6049
Philip Vieira <philip@vallin.se>
2013-02-19 16:09:20 +0100

    Fetch assets through ssl.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Cloudsdale::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://assets.cloudsdale.org"
+  config.action_controller.asset_host = "https://c776950.ssl.cf2.rackcdn.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
