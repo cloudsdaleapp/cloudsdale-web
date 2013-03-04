d1e1b3b2e25191b50578f804821d26a11a2c05da
Philip Vieira <philip@vallin.se>
2013-03-05 00:55:52 +0100

    Added the base for rackspace cloud files upload.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,9 +27,10 @@ role :web,  "ovh.cloudsdale.org"
 role :app,  "ovh.cloudsdale.org", :primary => true
 
 after 'deploy', 'deploy:permissions:correct'
+# after 'deploy:assets:precompile', 'deploy:assets:upload'
 
 # Default Environment
-default_environment["RAILS_ENV"]    = 'production'
+default_environment["RAILS_ENV"]    = rails_env
 default_environment["PATH"]         = "/usr/local/rvm/gems/#{ruby_version}/bin:/usr/local/rvm/gems/#{ruby_version}@global/bin:/usr/local/rvm/rubies/#{ruby_version}/bin:/usr/local/rvm/gems/#{ruby_version}@#{application}/bin:/usr/local/rvm/gems/#{ruby_version}@global/bin:/usr/local/rvm/rubies/#{ruby_version}/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/#{ruby_version}"
 default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/#{ruby_version}@#{application}:/usr/local/rvm/gems/#{ruby_version}@global"
@@ -41,6 +42,13 @@ namespace :deploy do
   namespace :permissions do
     task :correct, :roles => :app, :except => { :no_release => true } do
       run "#{try_sudo} chown -R #{user}:#{group} /opt/app/#{application}"
+    end
+  end
+
+  namespace :assets do
+    desc "Deploy assets to Rackspace CloudFiles."
+    task :upload, :roles => :app, :except => { :no_release => true }, :only => { :primary => true } do
+      run "cd #{deploy_to}/current && /usr/bin/env rake assets:upload RAILS_ENV=#{rails_env} SHARED_PATH=#{shared_path}"
     end
   end
 
