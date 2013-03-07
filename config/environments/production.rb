d6cb033cfab5c4fd6756461ab22b7cd54594bdaa
Philip Vieira <philip@vallin.se>
2013-03-07 21:18:17 +0100

    use memcachestore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Cloudsdale::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = "https://c776950.ssl.cf2.rackcdn.com"
