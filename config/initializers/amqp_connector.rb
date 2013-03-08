c2d74b8f44de0fc6e5bc73bf56c6792b9bfb90c0
Philip Vieira <philip@vallin.se>
2013-03-08 12:18:03 +0100

    Try and reconnect to rabbitmq server when server-down received.

--- config/initializers/amqp_connector.rb
+++ config/initializers/amqp_connector.rb
@@ -1,6 +1,6 @@
 module AMQPConnector
 
-  def enqueue!(queue,data)
+  def enqueue!(queue,data,tries=3)
 
     encoded_data = data.class == String ? data : Yajl::Encoder.encode(data)
 
@@ -8,7 +8,8 @@ module AMQPConnector
     e = Cloudsdale.bunny.exchange('')
 
     e.publish(encoded_data, :key => queue.to_s, :content_type => "application/json")
-
+  rescue Bunny::ServerDownError
+    enqueue!(queue,data,(tries - 1)) if tries >= 1
   end
 
 end
