7e526c7b9da5bebf0444a2f6ddfec6de9270ca27
Philip Vieira <philip@vallin.se>
2013-03-28 22:38:25 +0100

    Updated route explanation comments to be very specific.
    Comments are ugly I KNOW, but if any new developer comes and joins this project, the rout file will not make much sense otherwise.

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,8 @@ Cloudsdale::Application.routes.draw do
 
     end
 
-    # Post update method for cloud.
+    # To do a form encoded request to update clouds we need to add a specific
+    # POST request to the clouds update path.
     match "/clouds/:id" => "clouds#update", via: :post, as: :cloud
 
 
@@ -50,51 +51,64 @@ Cloudsdale::Application.routes.draw do
 
     end
 
-    # Post update method for user.
+    # To do a form encoded request to update users we need to add a specific
+    # POST request to the users update path.
     match "/users/:id" => "users#update", via: :post, as: :user
 
+    # The API has it's own 404 response. This is here so that 404's will not
+    # be picked up by the website catch-all response and can return with
+    # Cloudsdale API's specific JSON response wrapper.
     match '*path', to: 'exceptions#routing_error'
 
   end
 
-  # Public: The front page of Cloudsdale.
+  # The root path where users will land when entering our url.
   root to: 'root#index'
 
-  # Public: Logout path
+  # Use this endpoint to end a users session through the webapp.
   match '/logout' => 'sessions#destroy', as: :logout
 
-  # Public: The maintenance page hosted by the Cloudsdale app.
+  # A maintenence page hosted by the Cloudsdale app itself, this is rarely
+  # used as Cloudsdale never has scheduled maintenences.
   match '/maintenance' => 'root#maintenance', as: :maintenance
 
-  # Internal: Used by Omniauth to redirect users back from authenticating.
+  # Omniauth routes to connect users to different services oAuth2 services
+  # like Twitter and Facebook. This has nothing to do with Cloudsdale's own
+  # oAuth2 provider.
   match '/auth/:provider/callback' => 'authentications#create'
   match '/auth/failure' => 'authentications#failure'
 
-  # Public: A place to explore Clouds
+  # The explore page is devided up in to three tiers where the front-end will
+  # automatically take you up to the default tier if you're requesting it at
+  # at a lower level.
   get '/explore' => "root#explore", as: :explore
   get '/explore/clouds' => "root#explore", as: :explore_by_resource
   get '/explore/clouds/:scope' => "root#explore", as: :explore_by_resource_and_scope
 
-  # Mobile: Terms and Conditions
+  # Mobile terms and conditions page, this route will most certenly be deprecated.
   match '/mobile/tncs' => 'mobile#tncs', as: :mobile_tncs
 
-  # Clouds
+  # Path straight to a Cloud, you can supply either the clouds ID or it's shorthand.
   match '/clouds/:id_or_short_name' => "clouds#show", as: :cloud
 
-  # Resource: For clouds.
-  # resources :clouds, :only => [:show]
   resources :users, :only => [] do
     match "/restore/:token" => "users#restore", as: :restore, on: :collection
   end
 
+  # PayPal Donation endpoints for PayPal own IPN to automatically update payments
+  # received by by our users to run scrips that connect the two.
   resources :donations, only: [] do
     get :success, on: :collection, as: :success
     get :cancel,  on: :collection, as: :cancel
     post :paypal_ipn, on: :collection, as: :paypal_ipn
   end
 
+  # The XML sitemap that google uses to index the Cloudsdale site. Currently
+  # lists all know pages as well as automatically listing all public Clouds.
   get '/sitemap' => 'sitemap#index', as: :sitemap
 
+  # A way to add a page through the pages controller without creating a specific
+  # route for it. This NEEDS to be last so that it's a "catch all" route.
   match '/:page_id' => 'pages#show', as: :page
 
 end
