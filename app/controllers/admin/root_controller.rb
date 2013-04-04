1de0273707f7e29a5a73e91f7f0f7017f4452490
Philip Vieira <philip@vallin.se>
2013-04-04 06:10:00 +0200

    Added a very basic admin controller.

--- app/controllers/admin/root_controller.rb
+++ app/controllers/admin/root_controller.rb
@@ -2,6 +2,12 @@ class Admin::RootController < AdminController
 
   def index
 
+    start_at = 30.days.ago.utc.beginning_of_day
+    end_at   = DateTime.now.utc.end_of_day
+
+    @first_date = (start_at.to_i * 1000)
+    @samples = Sample.where(:start_time.gte => start_at, :stop_time.lte => end_at)
+
   end
 
 end
