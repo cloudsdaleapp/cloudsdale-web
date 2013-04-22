9c79bf6b7467f68bb0617762bc9b779a9ba45d53
Philip Vieira <philip@vallin.se>
2013-04-22 22:48:38 +0200

    Set up carrierwave for development.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,20 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.delete_tmp_file_after_storage = true
-  config.fog_credentials = {
-    :provider           => 'Rackspace',
-    :rackspace_username => Cloudsdale.config['rackspace_cloudfiles']['username'],
-    :rackspace_api_key  => Cloudsdale.config['rackspace_cloudfiles']['api_key'],
-    :rackspace_servicenet => false
-  }
-  config.fog_directory = Cloudsdale.config['rackspace_cloudfiles']['fog_directory']
-  config.fog_host = Cloudsdale.config['rackspace_cloudfiles']['fog_host']
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+
+  if Rails.env.production?
+
+    config.root = Rails.root.join('tmp')
+    config.delete_tmp_file_after_storage = true
+    config.fog_credentials = {
+      :provider           => 'Rackspace',
+      :rackspace_username => Cloudsdale.config['rackspace_cloudfiles']['username'],
+      :rackspace_api_key  => Cloudsdale.config['rackspace_cloudfiles']['api_key'],
+      :rackspace_servicenet => false
+    }
+    config.fog_directory = Cloudsdale.config['rackspace_cloudfiles']['fog_directory']
+    config.fog_host = Cloudsdale.config['rackspace_cloudfiles']['fog_host']
+
+  end
+
 end
-
-
