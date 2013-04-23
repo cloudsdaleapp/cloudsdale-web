b40c47e14e701c948328ffd706a69e49556dc9e4
Philip Vieira <philip@vallin.se>
2013-04-23 12:16:52 +0200

    Refactored the avatar and preview uploader to inherit from the application uploader. #dry

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,30 +1,6 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
-
-  include CarrierWave::MiniMagick
-  include Sprockets::Helpers::RailsHelper
-
-  def config
-    Rails.application.config.action_controller
-  end
-
-  def controller
-    nil
-  end
-
-  # Choose what kind of storage to use for this uploader:
-  storage :fog
-
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "avatars/#{model.id}/"
-  end
+class AvatarUploader < ApplicationUploader
 
   # Process files as they are uploaded:
   process :resize_to_fill => [200, 200]
@@ -52,25 +28,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :convert => 'png'
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
   def filename
      "#{secure_token(10)}_avatar.png" if original_filename.present?
   end
 
   def default_url
     image_path("#{Cloudsdale.config['asset_url']}/assets/fallback/" + [mounted_as, version_name, "#{model.class.to_s.downcase}.png"].compact.join('_'))
-  end
-
-  protected
-
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
 
 end
