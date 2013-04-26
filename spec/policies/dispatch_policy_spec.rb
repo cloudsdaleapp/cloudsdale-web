788670376bf106018e6abe6dd06a32869692e5ba
Philip Vieira <philip@vallin.se>
2013-04-26 23:39:33 +0200

    Created a dispatch policy.

---
+++ spec/policies/dispatch_policy_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe DispatchPolicy do
+  subject { DispatchPolicy }
+  let(:dispatch) { build(:dispatch) }
+  let(:user)     { build(:user) }
+
+  permissions :create?, :update?, :index?, :show? do
+
+    [:admin,:developer,:founder].each do |role|
+      it "does permit a user of role :#{role}" do
+        user.role = User::ROLES[role]
+        should permit(user,dispatch)
+      end
+    end
+    [:normal,:donor,:legacy,:associate].each do |role|
+      it "does not permit a user of role :#{role}" do
+        user.role = User::ROLES[role]
+        should_not permit(user,dispatch)
+      end
+    end
+  end
+
+  permissions :destroy? do
+    it "does not permit any user to destroy a dispatch" do
+      should_not permit(user,dispatch)
+    end
+  end
+
+end
