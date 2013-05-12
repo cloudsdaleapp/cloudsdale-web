08cc6bf4bebd4452a5ef9888858d80eb8b429a4a
Philip Vieira <philip@vallin.se>
2013-05-12 18:56:55 +0200

    Make session work for all subdomains.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cloudsdale::Application.config.secret_token = '8948bbdacfe793a42f7714666fd3b79aba1653cfbc82548b4b1da254cd7896edc61e54dda378979f121a287c8a476d9642d368d0ab8ebc092d16f4992e211d12'
+Cloudsdale::Application.config.secret_token = 'a1f46b110691e6a31f760a4914140ad9643356948621f9ea947ea7a25cab4da6e6b50a10b893eba7e8eeb68f7a1135787dd462fc9d388d5d7e480d6e3c4421e1'
