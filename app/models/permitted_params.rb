8c1fda7ce73552c4d06297efd71675c186703f6f
Philip Vieira <philip@vallin.se>
2013-05-13 10:03:03 +0200

    Add Permitted params for Doorkeeper::Application

--- app/models/permitted_params.rb
+++ app/models/permitted_params.rb
@@ -6,6 +6,12 @@ class PermittedParams < Struct.new(:params, :user)
     PermittedSessionParams.new(self.params, self.user)
   end
 
+  # Public: Permitted Application parameters.
+  # Returns an instance of strong parameters
+  def application
+    PermittedApplicationParams.new(self.params, self.user)
+  end
+
   class PermittedSessionParams < Struct.new(:params, :user)
 
     def create
@@ -14,6 +20,22 @@ class PermittedParams < Struct.new(:params, :user)
 
     def permitted(attributes)
       params.require(:session).permit(*attributes)
+    end
+
+  end
+
+  class PermittedApplicationParams < Struct.new(:params, :user)
+
+    def create
+      self.permitted([:name, :redirect_uri])
+    end
+
+    def update
+      self.permitted([:name, :redirect_uri, :website, :description])
+    end
+
+    def permitted(attributes)
+      params.require(:application).permit(*attributes)
     end
 
   end
