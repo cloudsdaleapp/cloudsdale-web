3035ec3de0b89429d29f930b4980452f6f179117
Philip Vieira <philip@vallin.se>
2013-05-13 10:00:36 +0200

    make site routes mapper work as intended.

--- lib/routing/site_mapper.rb
+++ lib/routing/site_mapper.rb
@@ -1,10 +1,10 @@
 module ActionDispatch::Routing
   class Mapper
     def site(name, options = {}, &block)
-      subdomain = options[:subdomain] || 'www'
+      subdomain = options[:subdomain] || name.to_s
 
-      constraints subdomain: /(#{subdomain}|#{name})/ do
-        namespace(name.to_sym, module: name.to_sym, path: '/', &block)
+      constraints subdomain: /(#{subdomain})/ do
+        scope(name.to_sym, module: name.to_sym, path: '/', &block)
       end
 
     end
