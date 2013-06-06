b0074777c0195da0f5bc2ee935de71a686e54d0d
Philip Vieira <philip@vallin.se>
2013-06-06 21:40:45 +0200

    Change secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cloudsdale::Application.config.secret_token = 'a1f46b110691e6a31f760a4914140ad9643356948621f9ea947ea7a25cab4da6e6b50a10b893eba7e8eeb68f7a1135787dd462fc9d388d5d7e480d6e3c4421e1'
+Cloudsdale::Application.config.secret_token = 'z2f46b110691e6a31f760a4914140ad9643356948621f9ea947ea7a25cab4da6e6b50a10b893eba7e8eeb68f7a1135787dd462fc9d388d5d7e480d6e3c4421e1'
