b94d18d116a1585d94730f868e944fe0b4b061f3
Philip Vieira <philip@vallin.se>
2013-06-07 18:51:47 +0200

    Use dynamic avatar URL.

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -7,7 +7,7 @@ class CloudsController < ApplicationController
     authorize @cloud, :show?
 
     @page_title = @cloud.name
-    @page_image = @cloud.avatar.url
+    @page_image = @cloud.dynamic_avatar_url
     @page_description = @cloud.description
     @page_type = "cloudsdale:cloud"
     @page_url = @cloud.short_name.present? ? cloud_url(@cloud.short_name) : cloud_url(@cloud.id)
