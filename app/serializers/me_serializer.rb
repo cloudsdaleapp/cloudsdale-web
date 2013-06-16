3e0572e1feceac76081b478e6959e2121ce19f92
Philip Vieira <philip@vallin.se>
2013-06-16 19:20:32 +0200

    Change key name in me_serializer from using_default_avatar to default_avatar

--- app/serializers/me_serializer.rb
+++ app/serializers/me_serializer.rb
@@ -1,12 +1,12 @@
 class MeSerializer < UserSerializer
 
-  attributes :total_clouds, :read_terms, :using_default_avatar, :notifications, :email, :email_verified
+  attributes :total_clouds, :read_terms, :default_avatar, :notifications, :email, :email_verified
 
   def total_clouds
     object.cloud_ids.try(:size) || 0
   end
 
-  def using_default_avatar
+  def default_avatar
     not object.avatar.present?
   end
 
