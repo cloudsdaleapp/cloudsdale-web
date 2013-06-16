0b25b9a933e6114897f1611699bde09b694129bc
Philip Vieira <philip@vallin.se>
2013-06-16 17:57:27 +0200

    Provide an auth_token endpoint for v2 to use with v1.

--- app/controllers/api/v2/me_controller.rb
+++ app/controllers/api/v2/me_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V2::MeController < Api::V2Controller
 
-  doorkeeper_for :show
+  doorkeeper_for :show, :backport
 
   def show
     respond_with current_resource_owner, status: 200, serializer: MeSerializer, root: 'user', meta: {
@@ -10,6 +10,18 @@ class Api::V2::MeController < Api::V2Controller
       errors: [],
       notice: ""
     }
+  end
+
+  def auth_token
+    resp = {
+      auth_token: current_resource_owner.auth_token,
+      meta: {
+        status: 200,
+        errors: [],
+        notice: "Access to auth tokens is deprecated."
+      }
+    }
+    respond_with resp, status: 200
   end
 
 private
--- config/routes/api_v2.rb
+++ config/routes/api_v2.rb
@@ -6,7 +6,9 @@ Cloudsdale::Application.routes.draw do
   api version: 2 do
     root to: 'root#index'
     # resource :session, only: [:create]
-    resource :me, only: [:show], controller: :me
+    resource :me, only: [:show], controller: :me do
+      get :auth_token, on: :member
+    end
 
     match '*path', to: 'root#not_found', via: [:post,:get,:put,:patch,:delete]
   end
