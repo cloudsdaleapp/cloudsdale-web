5f8a43e240337896423f33ed87b27d36ef64e824
Philip Vieira <philip@vallin.se>
2013-06-17 09:33:35 +0200

    Remove old helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,5 @@
 module ApplicationHelper
 
-  def current_tab(expected_tab=:default)
-    params[:tab] == expected_tab
-  end
-
-  # Takes the flash key value and uses it to
-  # determine which alert class to be used
-  def to_type(flash_key)
-    case flash_key.to_sym
-      when :alert
-        "alert-warning"
-      when :error
-        "alert-error"
-      when :notice
-        "alert-info"
-      when :success
-        "alert-success"
-      else
-        flash_key.to_s
-    end
-  end
-
   def with_format(format, &block)
     old_formats = formats
     self.formats = [format]
