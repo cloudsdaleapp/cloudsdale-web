ccf1eecb6f69e71f4cc9ff2c3efd5969460612cd
Philip Vieira <philip@vallin.se>
2013-06-23 21:26:14 +0200

    Able to supply starts and ends at params to admin dashboard.

--- app/controllers/admin/root_controller.rb
+++ app/controllers/admin/root_controller.rb
@@ -2,8 +2,8 @@ class Admin::RootController < AdminController
 
   def index
 
-    start_at = 60.days.ago
-    end_at   = Date.today
+    start_at = params[:start_at].present? ? DateTime.parse(params[:start_at]) : 60.days.ago
+    end_at   = params[:end_at].present?   ? DateTime.parse(params[:end_at])   : Date.today
 
     @first_date = (start_at.to_i * 1000)
     @samples = Sample.where(:date.gte => start_at.to_date.to_s, :date.lte => end_at.to_date.to_s).order_by(date: :asc)
