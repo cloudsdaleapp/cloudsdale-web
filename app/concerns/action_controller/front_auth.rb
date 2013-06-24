32cd233750aea0ed21c37be9648e68614d0d6148
Philip Vieira <philip@vallin.se>
2013-06-24 21:18:42 +0200

    Refactored front_auth.

--- app/concerns/action_controller/front_auth.rb
+++ app/concerns/action_controller/front_auth.rb
@@ -4,22 +4,19 @@ module ActionController::FrontAuth
   extend ActiveSupport::Concern
 
   included do
-    helper_method :current_user
+    helper_method :current_user, :auth_token
   end
 
   def current_user
-
-    user_id    ||= session[:user_id]
-    auth_token ||= cookies[:auth_token] || request.headers['X-Auth-Token']
-
-    if user_id
-      return @current_user ||= User.find_or_initialize_by(_id: session[:user_id])
-    elsif auth_token
-      return @current_user ||= User.find_or_initialize_by(auth_token: auth_token)
+    if auth_token
+      @current_user ||= User.find_or_initialize_by(auth_token: auth_token)
     else
-      return @current_user ||= User.new
+      @current_user ||= User.new
     end
+  end
 
+  def auth_token
+    @auth_token ||= cookies.signed[:auth_token] || request.headers['X-Auth-Token']
   end
 
 end
