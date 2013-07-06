1888ec792eb605e92702f7e411543d7e674c868f
Philip Vieira <philip@vallin.se>
2013-07-06 23:33:34 +0200

    Optimised some endpoints for messages.

--- app/controllers/api/v1/clouds/chat/messages_controller.rb
+++ app/controllers/api/v1/clouds/chat/messages_controller.rb
@@ -3,10 +3,6 @@ class Api::V1::Clouds::Chat::MessagesController < Api::V1Controller
   include ActionView::Helpers::AssetTagHelper
   def controller;self;end;private(:controller)
 
-  before_filter do
-    @cloud = fetch_cloud(params[:cloud_id])
-  end
-
   # Fetches the 50 latest messages for a chat
   #
   # Examples
@@ -15,7 +11,8 @@ class Api::V1::Clouds::Chat::MessagesController < Api::V1Controller
   # Returns a collection of messages
   def index
 
-    @messages = @cloud.chat.messages.order_by([:timestamp,:desc]).limit(50).reverse
+    @messages = Cloud.unscoped.find(params[:cloud_id]).chat.messages
+    @messages.reverse
 
     render status: 200
 
@@ -33,11 +30,10 @@ class Api::V1::Clouds::Chat::MessagesController < Api::V1Controller
   # Returns the message that was sent.
   def create
 
-    # Autoprune - Fetches all the oldest messages and TRASHES THEM LIKE A BAWS!
-    # TODO: MAEK BUAUTIFULIER!
-    @cloud.chat.messages.old.each { |message| message.destroy } if @cloud.chat.messages.count > 50
+    @cloud = Cloud.unscoped.without("chat.messages.author_id","chat.messages.drops").find(params[:cloud_id])
+    @cloud.chat.messages.old.destroy
 
-    @message = @cloud.chat.messages.build params[:message]
+    @message = @cloud.chat.messages.build(params[:message])
     @message.author = current_user
 
     authorize @message, :create?
@@ -53,22 +49,6 @@ class Api::V1::Clouds::Chat::MessagesController < Api::V1Controller
       render status: 422
     end
 
-  end
-
-private
-
-  # Private: Fetch cloud unscoped to include all chat messages.
-  #
-  # id - The ID of the Cloud to fetch.
-  #
-  # Examples
-  #
-  # fetch_cloud("...")
-  # # => <Cloud ...>
-  #
-  # Returns the user if a user is present
-  def fetch_cloud(id)
-    @cloud ||= Cloud.unscoped.find(id)
   end
 
 end
