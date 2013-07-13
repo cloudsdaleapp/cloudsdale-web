d97aaa6abbad65417c17fbe48045f780fee57959
Philip Vieira <philip@vallin.se>
2013-07-13 23:44:10 +0200

    Slim down MeController.

--- app/controllers/api/v2/me_controller.rb
+++ app/controllers/api/v2/me_controller.rb
@@ -5,15 +5,7 @@ class Api::V2::MeController < Api::V2Controller
   doorkeeper_for :show, :update, :get_auth_token, :if => ->{ current_user.new_record? }
 
   def show
-
-    @user = current_resource_owner
-
-    respond_with_resource(@user,
-      status:      200,
-      serializer:  MeSerializer,
-      root:        :user
-    )
-
+    respond_with_resource(current_resource_owner, serializer: MeSerializer, root: :user)
   end
 
   def update
@@ -22,36 +14,18 @@ class Api::V2::MeController < Api::V2Controller
 
     authorize(@user,:update?)
 
-    if @user.update_attributes(refine(@user,:update))
-      respond_with_resource(@user,
-        status:      200,
-        serializer:  MeSerializer,
-        root:        :user
-      )
+    if @user.update_attributes(params.for(@user))
+      respond_with_resource(@user, status: 200, serializer: MeSerializer, root: :user)
     else
       build_errors_from(@user)
-      respond_with_resource(@user,
-        status:      422,
-        serializer:  MeSerializer,
-        root:        :user
-      )
+      respond_with_resource(@user, status: 422, serializer: MeSerializer, root: :user)
     end
 
   end
 
   def get_auth_token
-
-    add_error(
-      type:    :warning,
-      message: "This resource is deprecated."
-    )
-
-    respond_with_resource(current_resource_owner,
-      status: 200,
-      serializer: Me::AuthTokenSerializer,
-      root: :user
-    )
-
+    add_error(type: :warning, message: "This resource is deprecated.")
+    respond_with_resource(current_resource_owner, serializer: Me::AuthTokenSerializer, root: :user)
   end
 
 end
