ca0b17eb60790e60d8baeca1811383785a2f569c
Philip Vieira <philip@vallin.se>
2013-07-14 01:53:19 +0200

    Fix'd

--- db/migrate/20130713231834_put_conversations_in_their_own_collection.rb
+++ db/migrate/20130713231834_put_conversations_in_their_own_collection.rb
@@ -3,7 +3,7 @@ class PutConversationsInTheirOwnCollection < Mongoid::Migration
   def self.up
     User.all.unset(:conversations)
     User.all.includes(:clouds).each do |user|
-      user.clouds.each { |cloud| Conversation.as(user, about: cloud).start }
+      user.clouds.each { |cloud| c = Conversation.as(user, about: cloud); c.start if c.new_record? }
       print '.'
     end
     puts "\n"
