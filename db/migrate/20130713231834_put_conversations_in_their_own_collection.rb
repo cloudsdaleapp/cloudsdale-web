5c3ca2b9945257f0d85e98020cb9c8bc5c7d9685
Philip Vieira <philip@vallin.se>
2013-07-14 02:40:13 +0200

    Invalidate, it has run.

--- db/migrate/20130713231834_put_conversations_in_their_own_collection.rb
+++ db/migrate/20130713231834_put_conversations_in_their_own_collection.rb
@@ -1,10 +1,15 @@
 class PutConversationsInTheirOwnCollection < Mongoid::Migration
 
   def self.up
-    User.all.unset(:conversations)
-    User.all.includes(:clouds).each do |user|
-      user.clouds.each { |cloud| c = Conversation.as(user, about: cloud); c.start if c.new_record? }
-    end
+    # User.all.unset(:conversations)
+    # User.all.includes(:clouds,:conversations).each do |user|
+    #   user.clouds.each do |cloud|
+    #     c = Conversation.as(user, about: cloud)
+    #     if c.new_record?
+    #       c.start
+    #       print "."
+    #     end
+    # end
   end
 
   def self.down
