e1ac1aa13eb746659116c5ef490b64f1e161d8e6
Philip Vieira <philip@vallin.se>
2013-08-21 01:33:18 +0200

    Serializers for Users.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,13 @@
 class UserSerializer < ApplicationSerializer
 
+  cached true
+
   attribute :name,          key: :display_name
   attribute :symbolic_role, key: :role_name
 
   attributes :username, :role, :avatar, :suspended
+
+  attribute :refs
 
   def avatar
     avatar_id_type = object.email_hash.present? ? :email_hash : :id
@@ -12,6 +16,14 @@ class UserSerializer < ApplicationSerializer
 
   def suspended
     object.banned?
+  end
+
+  def cache_key
+    [object.id, object.updated_at.utc.to_s(:number)].join("-")
+  end
+
+  def refs
+    [{ rel: 'self', href: v2_user_url(object.id, format: :json, host: $api_host) }]
   end
 
 end
