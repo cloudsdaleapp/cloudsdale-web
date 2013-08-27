d67abcd7add07e7f8506bae136ecc6d80eb4b5d9
Philip Vieira <philip@vallin.se>
2013-08-27 05:56:11 +0200

    Skip that shitty migration.

--- db/migrate/20130827011033_fix_cloud_short_names.rb
+++ db/migrate/20130827011033_fix_cloud_short_names.rb
@@ -1,16 +1,6 @@
 class FixCloudShortNames < Mongoid::Migration
 
   def self.up
-
-    Cloud.nor(short_name: nil).nor(short_name: "").each do |cloud|
-      cloud.short_name = cloud.short_name.gsub("-","_")
-      cloud.save
-    end
-
-    Cloud.or(short_name: nil).or(short_name: "").each do |cloud|
-      cloud.save
-    end
-
   end
 
   def self.down
