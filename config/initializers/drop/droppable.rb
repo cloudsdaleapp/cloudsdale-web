3a1ce60c68b7007cb37c8856c2a5c77d5d0252dd
Philip Vieira <philip@vallin.se>
2013-10-03 02:16:11 +0100

    Droppable class makes more sense.

--- config/initializers/drop/droppable.rb
+++ config/initializers/drop/droppable.rb
@@ -1,15 +1,11 @@
 module Droppable
 
   def create_drop_deposit_from_url_by_user(url,user)
-    d = Drop.find_or_initialize_from_matched_url(url)
-    i = self[:_id]
-    t = self[:_type].classify
-    if not d.nil? and d.save
-      dp = d.deposits.find_or_initialize_by(depositable_id: i, depositable_type: t)
-      dp.depositer_ids << user.id unless dp.depositer_ids.include?(user.id)
-      dp.save
-    end
-    d
+    drop = Drop.find_or_initialize_from_matched_url(url)
+    id   = self[:_id]
+    type = self[:_type].classify
+    drop.deposits.find_or_initialize_by(depositable_id: id, depositable_type: type).save if not drop.nil? and drop.save
+    return drop
   end
 
 end
