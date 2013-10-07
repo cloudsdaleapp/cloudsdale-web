5deb648e3e0cc646998c011042105f676889dd36
Philip Vieira <philip@vallin.se>
2013-10-07 16:45:22 +0200

    Fix geoactivity.

--- app/models/geo_activity.rb
+++ app/models/geo_activity.rb
@@ -5,26 +5,32 @@ class GeoActivity
   embedded_in :user
 
   field :_id,          type: String
-  field :date,         type: String,   default: -> { DateTime.current.utc.to_date }
+  field :dates,        type: Array,    default: -> { [DateTime.current.utc.to_date.to_s] }
   field :loc,          type: Array,    default: [ 0.0, 0.0 ]
   field :country_code, type: String
   field :city,         type: String
 
   def self.record(ip: nil, user: nil)
-    date     = DateTime.current.utc.to_date
-    activity = user.geo_activities.find_or_initialize_by(_id: ip, date: date) do |activity|
+    date     = DateTime.current.utc.to_date.to_s
+
+    activity = user.geo_activities.where(_id: ip).first
+    activity ||= user.geo_activities.new
+
+    if activity
       geo                   = $geoip.look_up(ip)
-      activity.id           = ip
-      activity.city         = geo[:city]
-      activity.country_code = geo[:country_code3]
-      activity.loc          = [ geo[:longitude], geo[:latitude] ]
+      activity.id           ||= ip
+      activity.city         ||= geo[:city]
+      activity.country_code ||= geo[:country_code3]
+      activity.loc          ||= [ geo[:longitude], geo[:latitude] ]
+      activity.dates        << date unless activity.dates.include?(date)
     end
+
     return activity
   end
 
   def self.record!(ip: nil, user: nil)
     activity = self.record(ip: ip, user: user)
-    activity.save if activity.new_record?
+    activity.save if activity.new_record? || activity.changed?
     return activity
   end
 
