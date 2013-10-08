77033d1961ccb4ca988442c0c37ccfcbc5ff77f0
Philip Vieira <philip@vallin.se>
2013-10-08 04:31:23 +0200

    Add more allowed attributes to clouds refinery.

--- app/refineries/cloud_refinery.rb
+++ app/refineries/cloud_refinery.rb
@@ -1,10 +1,11 @@
 class CloudRefinery < ApplicationRefinery
 
   def create
-    [:name, :short_name, :description]
+    [:name, :short_name, :description, :hidden, :locked, :remote_avatar_url, :avatar, :rules]
   end
 
   def update
+    create + [:remove_avatar]
   end
 
 end
