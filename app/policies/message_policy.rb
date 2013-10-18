39b220d06586e5b877d22299c307d0182f50133f
Philip Vieira <philip@vallin.se>
2013-10-18 03:17:04 +0200

    Update policy to account for editing.

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -9,6 +9,7 @@ class MessagePolicy < ApplicationPolicy
   def create?
     (user_is_not_banned_from_topic? and
     user_is_member_of_topic? and
+    user_is_author? and
     topic_is_unlocked?) or
     user.is_of_role? :admin
   end
@@ -18,6 +19,10 @@ class MessagePolicy < ApplicationPolicy
   end
 
 private
+
+  def user_is_author?
+    record.author.id == user.id
+  end
 
   def user_is_member_of_topic?
     record.topic.user_ids.include? user.id
