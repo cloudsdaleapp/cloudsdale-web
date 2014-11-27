2c3a87d77119d7df21e7ad17cbd9d194966de6e9
Philip Vieira <philip@vallin.se>
2014-11-27 02:13:05 +0000

    Reconfigure Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 ENV["RAILS_ENV"] ||= "production"
 
-cores = if RUBY_PLATFORM.match(/darwin/i)
+total_workers = if RUBY_PLATFORM.match(/darwin/i)
   `sysctl hw.ncpu | awk '{print $2}'`.chomp
 elsif RUBY_PLATFORM.match(/linux/i)
   `nproc`.chomp
@@ -9,8 +9,7 @@ else
 end
 
 listen (ENV["PORT"] || 8080), backlog: 1024
-
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || cores)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || total_workers)
 
 if ENV["RAILS_ENV"] == "development"
   timeout 10000
@@ -18,15 +17,22 @@ if ENV["RAILS_ENV"] == "development"
 else
   timeout 15
   preload_app true
-  stderr_path "log/unicorn.error.log"
-  stdout_path "log/unicorn.access.log"
+  pid "/tmp/unicorn.pid"
 end
 
 before_fork do |server, worker|
+  old_pid = "#{ server.config[:pid] }.old"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
   Signal.trap("TERM") { Process.kill("QUIT", Process.pid) }
 end
 
 after_fork do |server, worker|
   Signal.trap("TERM") { Process.kill("QUIT", Process.pid) }
+  ::NewRelic::Agent.after_fork(force_reconnect: true) if defined?(Unicorn) && ENV["RAILS_ENV"] == "production"
 end
 
